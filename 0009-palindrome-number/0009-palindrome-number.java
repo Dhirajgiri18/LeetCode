1a8356fe911107c9c778ef5acddc770cf3f1c625
DHIRAJ GIRI <dhirajgiri91124@gmail.com>
2024-07-24 18:37:11 +0530

    Time: 5 ms (71.54%), Space: 43.7 MB (68.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x==0) return true;
+        if(x==0){
+            return true;
+        }
         if(x<0 || x%10 == 0){
             return false;
         }
