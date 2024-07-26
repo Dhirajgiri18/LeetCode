4c034c66b6626d16862aa96bae1af347ef4ac4db
DHIRAJ GIRI <dhirajgiri91124@gmail.com>
2024-07-26 12:10:04 +0530

    Time: 23 ms (5.86%), Space: 44.9 MB (20.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,5 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
         int count =0;
 
         for(int i=0; i<nums.length; i++){
@@ -10,6 +13,10 @@ class Solution {
                 count++;
             }
         }
+        for (int i = 0; i < count; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
         return count;     
     }
 }
